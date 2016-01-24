a1b28bad9959fa696971a8f0bf7db3245d35881e
John Stovin <john.d.stovin@gmail.com>
2016-01-24 13:19:39 +0000

    Face tracking

--- MissileCommander/MainPage.xaml.cs
+++ MissileCommander/MainPage.xaml.cs
@@ -8,8 +8,15 @@ using Windows.ApplicationModel;
 using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
 using Windows.Media.Capture;
+using Windows.Media.Core;
+using Windows.Media.FaceAnalysis;
+using Windows.Media.MediaProperties;
 using Windows.System.Display;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,6 +24,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,6 +37,7 @@ namespace MissileCommander
     {
         private MediaCapture mediaCapture;
         private readonly DisplayRequest displayRequest = new DisplayRequest();
+        private FaceDetectionEffect faceDetectionEffect;
 
         public MainPage()
         {
@@ -73,7 +82,131 @@ namespace MissileCommander
                 }
 
                 await StartPreviewAsync();
+                await StartFaceDetectionAsync();
+
             }
+        }
+
+        private async Task StartFaceDetectionAsync()
+        {
+            // Create the definition, which will contain some initialization settings
+            var definition = new FaceDetectionEffectDefinition();
+
+            // To ensure preview smoothness, do not delay incoming samples
+            definition.SynchronousDetectionEnabled = false;
+
+            // In this scenario, choose detection speed over accuracy
+            definition.DetectionMode = FaceDetectionMode.HighPerformance;
+
+            // Add the effect to the preview stream
+            faceDetectionEffect = (FaceDetectionEffect)await mediaCapture.AddVideoEffectAsync(definition, MediaStreamType.VideoPreview);
+
+            // Register for face detection events
+            faceDetectionEffect.FaceDetected += FaceDetectionEffect_FaceDetected;
+
+            // Choose the shortest interval between detection events
+            faceDetectionEffect.DesiredDetectionInterval = TimeSpan.FromMilliseconds(33);
+
+            // Start detecting faces
+            faceDetectionEffect.Enabled = true;
+        }
+
+        private async void FaceDetectionEffect_FaceDetected(FaceDetectionEffect sender, FaceDetectedEventArgs args)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => HighlightDetectedFaces(args.ResultFrame.DetectedFaces));
+        }
+
+        private void HighlightDetectedFaces(IReadOnlyList<DetectedFace> faces)
+        {
+            FacesCanvas.Children.Clear();
+
+            // For each detected face
+            for (int i = 0; i < faces.Count; i++)
+            {
+                // Face coordinate units are preview resolution pixels, which can be a different scale from our display resolution, so a conversion may be necessary
+                Rectangle faceBoundingBox = ConvertPreviewToUiRectangle(faces[i].FaceBox);
+
+                // Set bounding box stroke properties
+                faceBoundingBox.StrokeThickness = 2;
+
+                // Highlight the first face in the set
+                faceBoundingBox.Stroke = (i == 0 ? new SolidColorBrush(Colors.Blue) : new SolidColorBrush(Colors.DeepSkyBlue));
+
+                // Add grid to canvas containing all face UI objects
+                FacesCanvas.Children.Add(faceBoundingBox);
+            }
+        }
+
+        private Rectangle ConvertPreviewToUiRectangle(BitmapBounds faceBoxInPreviewCoordinates)
+        {
+            var result = new Rectangle();
+
+            var previewStream = mediaCapture.VideoDeviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview) as VideoEncodingProperties;
+
+            // If there is no available information about the preview, return an empty rectangle, as re-scaling to the screen coordinates will be impossible
+            if (previewStream == null)
+                return result;
+
+            // Similarly, if any of the dimensions is zero (which would only happen in an error case) return an empty rectangle
+            if (previewStream.Width == 0 || previewStream.Height == 0)
+                return result;
+
+            double streamWidth = previewStream.Width;
+            double streamHeight = previewStream.Height;
+
+            // Get the rectangle that is occupied by the actual video feed
+            var previewInUI = GetPreviewStreamRectInControl(previewStream, PreviewControl);
+
+            // Scale the width and height from preview stream coordinates to window coordinates
+            result.Width = (faceBoxInPreviewCoordinates.Width / streamWidth) * previewInUI.Width;
+            result.Height = (faceBoxInPreviewCoordinates.Height / streamHeight) * previewInUI.Height;
+
+            // Scale the X and Y coordinates from preview stream coordinates to window coordinates
+            var x = (faceBoxInPreviewCoordinates.X / streamWidth) * previewInUI.Width;
+            var y = (faceBoxInPreviewCoordinates.Y / streamHeight) * previewInUI.Height;
+            Canvas.SetLeft(result, x);
+            Canvas.SetTop(result, y);
+
+            return result;
+        }
+
+        public Rect GetPreviewStreamRectInControl(VideoEncodingProperties previewResolution, CaptureElement previewControl)
+        {
+            var result = new Rect();
+
+            // In case this function is called before everything is initialized correctly, return an empty result
+            if (previewControl == null || previewControl.ActualHeight < 1 || previewControl.ActualWidth < 1 ||
+                previewResolution == null || previewResolution.Height == 0 || previewResolution.Width == 0)
+            {
+                return result;
+            }
+
+            var streamWidth = previewResolution.Width;
+            var streamHeight = previewResolution.Height;
+
+            // Start by assuming the preview display area in the control spans the entire width and height both (this is corrected in the next if for the necessary dimension)
+            result.Width = previewControl.ActualWidth;
+            result.Height = previewControl.ActualHeight;
+
+            // If UI is "wider" than preview, letterboxing will be on the sides
+            if ((previewControl.ActualWidth / previewControl.ActualHeight > streamWidth / (double)streamHeight))
+            {
+                var scale = previewControl.ActualHeight / streamHeight;
+                var scaledWidth = streamWidth * scale;
+
+                result.X = (previewControl.ActualWidth - scaledWidth) / 2.0;
+                result.Width = scaledWidth;
+            }
+            else // Preview stream is "wider" than UI, so letterboxing will be on the top+bottom
+            {
+                var scale = previewControl.ActualWidth / streamWidth;
+                var scaledHeight = streamHeight * scale;
+
+                result.Y = (previewControl.ActualHeight - scaledHeight) / 2.0;
+                result.Height = scaledHeight;
+            }
+
+            return result;
         }
 
         private async Task StartPreviewAsync()
